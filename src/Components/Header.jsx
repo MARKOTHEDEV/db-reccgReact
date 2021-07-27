a1ab58f518065a1ab70d4946a4071f2d7d129c1a
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-27 19:05:02 +0100

    working on home

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -3,19 +3,24 @@ import Navbar from "./Navbar";
 
 const Header = ({ bgImage, homeTitle, homeDesc }) => {
   return (
-    <div className="header flex flex-col justify-between py-12" style={{background: `url(${bgImage})`}}>
-     <div className="">
-     <Navbar />
-     </div>
+    <div
+      className="header flex flex-col justify-between py-12"
+      style={{ background: `url(${bgImage})`, backgroundRepeat: "no-repeat", backgroundPosition: "center", backgroundSize: "cover" }}
+    >
+      <div className="">
+        <Navbar />
+      </div>
 
-     <div className="myContainer text-white">
-            <div className="grid grid-cols-1 lg:grid-cols-2">
-                 <div>
-                 <h2 className="font-black text-2xl md:text-4xl">{homeTitle}</h2>
-            <p className="font-semibold tracking-wider text-base text-center">{homeDesc}</p>
-                 </div>
-            </div>
-     </div>
+      <div className="myContainer text-white">
+        <div className="grid grid-cols-1 lg:grid-cols-2">
+          <div>
+            <h2 className="font-black text-2xl md:text-4xl">{homeTitle}</h2>
+            <p className="font-semibold tracking-wider text-base text-center">
+              {homeDesc}
+            </p>
+          </div>
+        </div>
+      </div>
     </div>
   );
 };
--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,11 +1,17 @@
-import React from 'react'
+import React from "react";
+import Header from "../Components/Header";
+import heroBg2 from "../Images/hero2.png"
 
 const About = () => {
-    return (
-        <div>
-            About
-        </div>
-    )
-}
+  return (
+    <div>
+      <Header
+        bgImage={heroBg2}
+        homeTitle="About us"
+        
+      />
+    </div>
+  );
+};
 
-export default About
+export default About;
