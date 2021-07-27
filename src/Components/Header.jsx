97de28854a36e9065a4dfc1abf23bd54b47e4632
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-27 06:07:26 +0100

    working on nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,18 @@
 import React from "react";
 import Navbar from "./Navbar";
 
-const Header = ({ bgImage }) => {
+const Header = ({ bgImage, homeTitle, homeDesc }) => {
   return (
-    <div className="header">
-     <div>
-     <Navbar />
+    <div className="header flex flex-col justify-between py-12" style={{background: `url(${bgImage})`}}>
+     <div className="">
+     {/* <Navbar /> */}
+     </div>
+
+     <div className="myContainer text-white hidden">
+            <div className="hidden">
+            <h2 className="font-black text-2xl md:text-4xl">{homeTitle}</h2>
+            <p className="font-semibold tracking-wider text-base">{homeDesc}</p>
+            </div>
      </div>
     </div>
   );
--- src/Pages/Home.jsx
+++ src/Pages/Home.jsx
@@ -5,7 +5,8 @@ import heroBg1 from "../Images/heroHome.png"
 const Home = () => {
     return (
         <div>
-           <Header bgImage={heroBg1}/>
+           <Header bgImage={heroBg1} homeTitle="Worship with us" homeDesc="Join us live Sunday @ 9:00am - 10:30am (1st Service), 11:00am - 12:30pm (2nd Service) Combine Service every first Sunday of the month @ 10:00am"/>
+           <br /> <br />
         </div>
     )
 }
