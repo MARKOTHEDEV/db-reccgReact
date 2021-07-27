482328acd2c825d6002cd6c4eecbe36292dd45fa
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-27 04:50:45 +0100

    working on nav

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -4,10 +4,10 @@ import Navbar from "./Navbar";
 const Header = ({ bgImage }) => {
   return (
     <div
-      className="header bg-cover bg-center bg-no-repeat relative"
+      className="h-screen w-full bg-cover bg-center bg-no-repeat relative"
       style={{ backgroundImage: `url(${bgImage})` }}
     >
-     <div className="absolute top-14">
+     <div>
      <Navbar />
      </div>
     </div>
