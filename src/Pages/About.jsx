4965838b7c616a983763ec350331e9c80074adb6
Onyekachi Godswill <omenukoonyekachi690@gmail.com>
2021-07-27 19:37:45 +0100

    working on home

--- src/Pages/About.jsx
+++ src/Pages/About.jsx
@@ -1,15 +1,27 @@
 import React from "react";
 import Header from "../Components/Header";
-import heroBg2 from "../Images/hero2.png"
+import heroBg2 from "../Images/hero2.png";
+import about from "../Images/about.jpg"
 
 const About = () => {
   return (
     <div>
-      <Header
-        bgImage={heroBg2}
-        homeTitle="About us"
-        
-      />
+      <Header bgImage={heroBg2} homeTitle="About us" />
+
+      <div  style={{
+                  background: `url(${about})`,
+                  backgroundRepeat: "no-repeat",
+                  backgroundPosition: "center",
+                  backgroundSize: "cover",
+                }}>
+        <div className="myContainer">
+            <div className="grid grid-cols-1 lg:grid-cols-3">
+                <div>
+
+                </div>
+            </div>
+        </div>
+      </div>
     </div>
   );
 };
