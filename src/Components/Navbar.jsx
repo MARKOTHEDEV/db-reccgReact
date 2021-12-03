f717fe945405d0c62eab11c7db8a5916be357f89
MARKOTHEDEV <ogechuwkumatthew@gmail.com>
2021-12-03 03:52:03 -0600

    added some few logic and some styling to the Navbar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -2,11 +2,12 @@ import React, { useState } from "react";
 import { NavLink } from "react-router-dom";
 import "./navbar.css";
 import logo from "../Images/logo.png";
-
+import { Link } from "react-router-dom";
 const Navbar = () => {
   const [isMobile, setIsMobile] = useState(false);
 
   const [NavFix, setNavFix] = useState(false);
+  const [liveStreamBtn,setLiveStreamBtn] = useState(false)
 
   const fixedNav = () => {
     if (window.scrollY >= 25) {
@@ -84,8 +85,13 @@ const Navbar = () => {
             ></path>
           </svg>
           <div>
-            <span className="block font-light">First Service:</span>
-            <span>9:00am – 10:30pm</span>
+            <span className="block font-light">ADDRESS:</span>
+            <span>5151 S Princeton Ave Chicago IL</span> <br />
+          </div>
+          <div>
+            <span className="block font-light">PO Box:</span>
+            <span>PO Box 17004 Chicago IL</span>
+
           </div>
         </div>
       </div>
@@ -133,8 +139,19 @@ const Navbar = () => {
             {" "}
             <li>Resources</li>{" "}
           </NavLink>
+
           <NavLink
-            to="/workProgress"
+            to="/gallery"
+            className="nav_link"
+            activeClassName="is-active"
+          >
+            {" "}
+            <li>Our Gallery</li>{" "}
+          </NavLink>
+
+
+          <NavLink
+            to="/events"
             className="nav_link"
             activeClassName="is-active"
           >
@@ -149,11 +166,53 @@ const Navbar = () => {
             {" "}
             <li>Contact Us</li>{" "}
           </NavLink>
-
-          <a href="https://www.youtube.com/c/RCCGHouseofHisPresenceChicago" target="_blank" rel="noreferrer" className="liveStream">
+          {/* https://www.youtube.com/c/RCCGHouseofHisPresenceChicago */}
+          {/* <a href="" target="_blank" rel="noreferrer" >
            
             <li>Live stream</li>{" "}
-          </a>
+          </a> */}
+
+
+
+
+<div class="relative inline-block text-left">
+  <div>
+    <button onClick={(e)=>setLiveStreamBtn(!liveStreamBtn)} type="button" 
+    class="inline-flex justify-center w-full rounded-md border border-gray-300 shadow-sm px-4 py-2 bg-white text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-100 focus:ring-indigo-500" 
+    id="menu-button" aria-expanded="true" aria-haspopup="true"
+            style={{backgroundColor:"#080b4b",color:"whitesmoke"}}
+    >
+      Live Stream
+      <svg class="-mr-1 ml-2 h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="flase">
+        <path fill-rule="evenodd" d="M5.293 7.293a1 1 0 011.414 0L10 10.586l3.293-3.293a1 1 0 111.414 1.414l-4 4a1 1 0 01-1.414 0l-4-4a1 1 0 010-1.414z" clip-rule="evenodd" />
+      </svg>
+    </button>
+  </div>
+
+
+  <div 
+  class="origin-top-right absolute right-0 mt-2 w-56 rounded-md shadow-lg bg-white ring-1 ring-black ring-opacity-5 focus:outline-none" 
+  role="menu" aria-orientation="vertical" aria-labelledby="menu-button" tabindex="-1" style={{display:`${liveStreamBtn?"block":"none"}`}}>
+    <div class="py-1" role="none">
+      <Link 
+      to="/archive" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">
+        Archive
+      </Link>
+
+      <a href="https://www.youtube.com/c/RCCGHouseofHisPresenceChicago" class="text-gray-700 block px-4 py-2 text-sm" role="menuitem" tabindex="-1" id="menu-item-0">
+          Live Stream 
+      </a>
+      
+    </div>
+  </div>
+</div>
+
+
+
+
+
+
+
         </ul>
         <div
           className="mobile-menu-icon md:pr-12 pt-1"
